715fbd22fef23b308726f88eb2c80606b667df8a
liuhepeng <liuhepeng@besttmkj.com>
2018-05-23 11:41:58 +0800

    配置属性

---
+++ File-Service/src/main/java/com/lhp/files/Files.java
@@ -0,0 +1,13 @@
+package com.lhp.files;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class Files {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
